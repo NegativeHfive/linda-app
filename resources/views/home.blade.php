50031f79053f9b9b19800ed5262aa1f47abd3268
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-05-24 18:18:56 +0200

    added gsap and started the programming

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('home.css')}}">
+</head>
+<body>
+    
+    <div class="navbar">
+        <h2>negative one</h2>
+
+        <div class="navbarlinks">
+            <div class="homelink">
+                <ion-icon name="bed-outline"></ion-icon>
+                <a href="#">Home</a>
+            </div>
+            <div class="aboutuslink">
+                <ion-icon name="reader-outline"></ion-icon>
+                <a href="#">About Us</a>
+            </div>
+            <div class="loginlink">
+                <ion-icon name="person-outline"></ion-icon>
+                <a href="#">Log Out</a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
